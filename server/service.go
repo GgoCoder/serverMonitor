c154ff78973a091876b02b0c9d8dd64b23b284b1
root <root@rguo-home.localdomain>
2022-09-04 16:44:59 +0800

    add web controller

--- server/service.go
+++ server/service.go
@@ -1,20 +1,48 @@
 package server
 
 import (
+	"serverMonitor/asset"
+	"serverMonitor/monitor"
+	"time"
+
 	"github.com/gin-gonic/gin"
 )
 
 func ListService(c *gin.Context) {
-	ResponseWithJson(c, "welcome to home")
+	services := monitor.ListService()
+	ResponseWithJson(c, services)
 }
 
 func AddService(c *gin.Context) {
+	service := &asset.Service{}
+	service.CreateTime = time.Now()
+	err := c.BindJSON(service)
+	if err != nil {
+		ResponseWithError(c, "error body")
+		return
+	}
+	monitor.AddService(service)
+	ResponseWithJson(c, "post service successfully")
 }
 
 func UpdateService(c *gin.Context) {
-
+	service := &asset.Service{}
+	err := c.BindJSON(service)
+	if err != nil {
+		ResponseWithError(c, "error body")
+		return
+	}
+	monitor.UpdateService(service)
+	ResponseWithJson(c, "update service successfully")
 }
 
 func DeleteService(c *gin.Context) {
-
+	service := &asset.Service{}
+	err := c.BindJSON(service)
+	if err != nil {
+		ResponseWithError(c, "error body")
+		return
+	}
+	monitor.DeleteService(service)
+	ResponseWithJson(c, "delete service successfully")
 }
